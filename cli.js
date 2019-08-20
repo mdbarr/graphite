16cd64558517d443b5705d4620f13c3b15c484f8
Mark Barr <mark@hyperingenuity.com>
2019-08-20 10:05:36 -0400

    cli: command line option handling added via minimist

--- cli.js
+++ cli.js
@@ -2,7 +2,11 @@
 'use strict';
 
 const Griff = require('./index');
-const griff = new Griff({ save: true });
+const minimist = require('minimist');
+
+const options = Object.assign({ save: true }, minimist(process.argv.slice(2)));
+
+const griff = new Griff(options);
 
 griff.generate().
   catch((error) => {
