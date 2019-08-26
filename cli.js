455b0a7ff93f56846ec617f6edfa7d7c2b12cf48
Mark Barr <mbarr@veobot.com>
2019-08-25 20:31:02 -0600

    young-griff: output cli option added and improved save logic

--- cli.js
+++ cli.js
@@ -4,11 +4,21 @@
 const Griff = require('./index');
 const minimist = require('minimist');
 
-const options = Object.assign({ save: true }, minimist(process.argv.slice(2)));
+const options = minimist(process.argv.slice(2));
+if (options._.length === 1 && options._[0] === '-') {
+  options.output = true;
+} else if (options.save === undefined) {
+  options.save = true;
+}
 
 const griff = new Griff(options);
 
 griff.generate().
+  then((svg) => {
+    if (options.output) {
+      console.log(svg);
+    }
+  }).
   catch((error) => {
     console.log(error);
   });
